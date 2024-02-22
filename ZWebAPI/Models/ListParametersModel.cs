126d72011c858c65c116116d6634d8f08f48e352
Ricardo Zambon <ricardo.zambon@live.com>
2024-02-22 08:24:32 -0300

    feat: add list models

---
+++ ZWebAPI/Models/ListParametersModel.cs
@@ -0,0 +1,36 @@
+﻿using ZWebAPI.Interfaces;
+
+namespace ZWebAPI.Models
+{
+    /// <summary>
+    /// Parameters model to filter or sort lists.
+    /// </summary>
+    /// <seealso cref="ZWebAPI.Models.SummaryParametersModel" />
+    /// <seealso cref="ZWebAPI.Interfaces.IListParameters" />
+    public class ListParametersModel : SummaryParametersModel, IListParameters
+    {
+        /// <summary>
+        /// Gets or sets the start row.
+        /// </summary>
+        /// <value>
+        /// The start row.
+        /// </value>
+        public int StartRow { get; set; }
+
+        /// <summary>
+        /// Gets or sets the end row.
+        /// </summary>
+        /// <value>
+        /// The end row.
+        /// </value>
+        public int EndRow { get; set; }
+
+        /// <summary>
+        /// Gets or sets the sort.
+        /// </summary>
+        /// <value>
+        /// The sort.
+        /// </value>
+        public IDictionary<string, string> Sort { get; set; } = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
+    }
+}
---
+++ ZWebAPI/Models/SummaryParametersModel.cs
@@ -0,0 +1,19 @@
+﻿using ZWebAPI.Interfaces;
+
+namespace ZWebAPI.Models
+{
+    /// <summary>
+    /// Parameters model to filter summaries.
+    /// </summary>
+    /// <seealso cref="ZWebAPI.Interfaces.ISummaryParameters" />
+    public class SummaryParametersModel : ISummaryParameters
+    {
+        /// <summary>
+        /// Gets or sets the filters.
+        /// </summary>
+        /// <value>
+        /// The filters.
+        /// </value>
+        public IDictionary<string, object>? Filters { get; set; } = new Dictionary<string, object>(StringComparer.InvariantCultureIgnoreCase);
+    }
+}
